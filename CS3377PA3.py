7992b9aa17155fc720db5f0fca5ca32cee652979
Andy <andrewysu07@gmail.com>
2025-11-14 17:08:57 -0600

    Flask Installation

--- CS3377PA3.py
+++ CS3377PA3.py
@@ -1,1 +1,9 @@
-print('Hello World')
+from flask import Flask
+app = Flask("Derm AI Companion")
+
+@app.route('/')
+def home():
+    return "Hello, Flask is working!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
